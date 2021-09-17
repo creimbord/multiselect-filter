2e158df0d261443b581cb1255c75308f14040272
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2021-09-17 11:40:46 +0300

    feature(main-screen): Add and configure phone table view

---
+++ MultiselectFilter/Modules/Main/View/MainViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MainViewController.swift
+//  MultiselectFilter
+//
+//  Created by Родион on 03.09.2021.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let dataSource = MainDataSource()
+    
+    // MARK: - Views
+    private lazy var filterButton: UIBarButtonItem = {
+        let image = UIImage(systemName: "slider.horizontal.3")
+        return UIBarButtonItem(
+            image: image,
+            style: .plain,
+            target: self,
+            action: #selector(openFilter)
+        )
+    }()
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = dataSource
+        tableView.register(PhoneCell.self, forCellReuseIdentifier: "PhoneCell")
+        return tableView
+    }()
+    
+    // MARK: - Life cycle
+    override func loadView() {
+        super.loadView()
+        setupViews()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Actions
+private extension MainViewController {
+    @objc func openFilter() {
+        print("Opening filter...")
+    }
+}
+
+// MARK: - Setup views
+private extension MainViewController {
+    func setupViews() {
+        addSubviews()
+        setupConstraints()
+        view.backgroundColor = .white
+    }
+    
+    func addSubviews() {
+        view.addSubview(tableView)
+        navigationItem.rightBarButtonItem = filterButton
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+}
